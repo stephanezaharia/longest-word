3c594d1fb29d51a800963e97d335c49ebc9fa87e
Stephane Zaharia <stephane.zaharia@gmail.com>
2018-11-27 16:12:58 +0100

    dictionary test added

--- game.py
+++ game.py
@@ -12,7 +12,6 @@ class Game:
         """ This is the longest word implementation class
         """
         self.random_grid()
-        print(self.grid)
 
     def random_grid(self):
         """ add a random grid in object. Make sure every letter appears once
@@ -31,12 +30,12 @@ class Game:
         """
         if not word or len(word) > len(self.grid):
             return False
-        for letter in self.grid:
-            cnt = word.count(letter)
-            #print(letter,cnt)
-            if cnt > 1:
-                return False
+
         for letter in word:
-            if letter not in self.grid:
+            cnt = word.count(letter)
+            cntmax = self.grid.count(letter)
+            if letter not in self.grid or cnt>cntmax:
+                print ("wrong",letter)
                 return False
+
         return True
--- tests/test_game.py
+++ tests/test_game.py
@@ -45,3 +45,21 @@ class TestGame(unittest.TestCase):
             if new_game.grid.count(letter)!=1:
                 return False
         return True
+
+    def test_unknown_word_is_invalid(self):
+      new_game = Game()
+      new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+      self.assertIs(new_game.is_valid('FEUN'), False)
+
+
+    def test_is_valid1(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Force the grid to a test case:
+        self.assertIs(new_game.is_valid('EUREKA'), True)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # Make sure the grid remained untouched
+
+    def test_is_invalid1(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Force the grid to a test case:
+        self.assertIs(new_game.is_valid('SANDWICH'), False)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # Make sure the grid remained untouched
